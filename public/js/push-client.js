65c621771230d25d62744fd61a2b30733c0334d0
Roys Esdras <roysesdras@gmail.com>
2026-09-02 10:40:48 +0000

    chore: align all cache busting versions to v1208 and fix service worker registration cache busting

--- public/js/push-client.js
+++ public/js/push-client.js
@@ -31,7 +31,7 @@ class DigiPushClient {
     }
 
     try {
-      this.swRegistration = await navigator.serviceWorker.register('/sw.js?v=1202');
+      this.swRegistration = await navigator.serviceWorker.register('/sw.js?v=1208');
       console.log('[+] Service Worker registered with scope:', this.swRegistration.scope);
       try { this.swRegistration.update(); } catch (e) {}
 
