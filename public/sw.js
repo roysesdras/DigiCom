c731a116701b81dff6130d8f3b6ca695b897f356
Roys Esdras <roysesdras@gmail.com>
2026-09-03 00:22:19 +0000

    style: enhance admin announcement modal and premium confirmation button design

--- public/sw.js
+++ public/sw.js
@@ -2,7 +2,7 @@
  * DigiCom Service Worker - PWA Offline Support & Background Web Push Dispatcher
  */
 
-const CACHE_NAME = 'digicom-pwa-v1220';
+const CACHE_NAME = 'digicom-pwa-v1221';
 const MEDIA_CACHE_NAME = 'digicom-media-v1';
 const ASSETS_TO_CACHE = [
   '/',
